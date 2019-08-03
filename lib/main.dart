ac85b06c3f31ff23c3bc1ed57a277d3e6a3ea9d3
Andrew Bell <fiixed@gmail.com>
2019-08-03 11:23:30 -0500

    card styling

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class MyHomePage extends StatelessWidget {
       amount: 16.53,
       date: DateTime.now(),
     ),
-    
   ];
 
   @override
@@ -52,9 +51,28 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(
+                          tx.date.toString(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
